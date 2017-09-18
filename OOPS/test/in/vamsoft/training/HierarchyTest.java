a97a795be8962457fa531698736b0c725ff845a0
sudhachinna <sudhac1994.sc@gmail.com>
2017-09-18 17:31:41 +0530

    commit3

--- OOPS/test/in/vamsoft/training/HierarchyTest.java
+++ OOPS/test/in/vamsoft/training/HierarchyTest.java
@@ -1,11 +1,21 @@
 package in.vamsoft.training;
 
+import in.vamsoft.training.OOPs.Car;
+import in.vamsoft.training.OOPs.SportsCar;
+
 public class HierarchyTest {
 
 	public static void main(String[] args) {
-		SportsCar car=new SportsCar();
+
+		SportsCar car = new SportsCar("SUV");
+
+		Car c1 = new SportsCar();
+
+		System.out.println(c1);
+		
 		System.out.println(car);
-		//this is the vahicle project
+
+		car.start();
 
 	}
 
